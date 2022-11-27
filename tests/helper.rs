da4e8fe84d81ba264815a27cc40e463194a3f600
Orhan "aib" Kavrakoglu <aibok42@gmail.com>
2022-11-27 20:16:14 +0100

    Add a helper function for creating an app TW_IDENTITY

--- tests/helper.rs
+++ tests/helper.rs
@@ -1,4 +1,5 @@
-use twain::twain_h_ext::DSMEntryProc;
+use twain::twain_h::*;
+use twain::twain_h_ext::*;
 
 #[cfg(unix)]
 const DSM_FILE: &str = "ext/libtwaindsm.so";
@@ -16,6 +17,25 @@ pub fn load_twain_lib() -> TwainLib {
 	TwainLib {
 		_lib: lib,
 		dsm_entry,
+	}
+}
+
+pub fn get_app_identity(support_app2:bool) -> TW_IDENTITY {
+	TW_IDENTITY {
+		Id: 0,
+		Version: TW_VERSION {
+			MajorNum: env!("CARGO_PKG_VERSION_MAJOR").parse::<u16>().unwrap() as TW_UINT16,
+			MinorNum: env!("CARGO_PKG_VERSION_MINOR").parse::<u16>().unwrap() as TW_UINT16,
+			Language: TWLG_ENGLISH_USA as TW_UINT16,
+			Country: TWCY_USA as TW_UINT16,
+			Info: tw_str32(env!("CARGO_PKG_VERSION")),
+		},
+		ProtocolMajor: TWON_PROTOCOLMAJOR as TW_UINT16,
+		ProtocolMinor: TWON_PROTOCOLMINOR as TW_UINT16,
+		SupportedGroups: DG_IMAGE | DG_CONTROL | if support_app2 { DF_APP2 } else { 0 },
+		Manufacturer: tw_str32("Rust TWAIN Library"),
+		ProductFamily: tw_str32("Tests"),
+		ProductName: tw_str32("Integration Test"),
 	}
 }
 
